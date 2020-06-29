f9c2ea5f4471ebdaa381fe12f74c99f37be6c539
Pedro Santos <phsantosti@gmail.com>
2020-06-29 09:38:14 -0300

    Implementação de chamadas a API para listar, cadastrar e remover repositorios

--- src/App.js
+++ src/App.js
@@ -1,31 +1,57 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import api from "./services/api";
 
 import "./styles.css";
 
 function App() {
-  async function handleAddRepository() {
-    // TODO
-  }
+    const [repositories, setRepositories] = useState([]);
 
-  async function handleRemoveRepository(id) {
-    // TODO
-  }
+    useEffect(() => {
+        api.get('repositories').then(response => {
+            setRepositories(response.data);
+        });
+    }, []);
 
-  return (
-    <div>
-      <ul data-testid="repository-list">
-        <li>
-          Repositório 1
 
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
-      </ul>
 
-      <button onClick={handleAddRepository}>Adicionar</button>
-    </div>
-  );
+    async function handleAddRepository() {
+        const response = await api.post('repositories', {
+            title: 'Projeto HTML5 S',
+            url: 'https://github.com/phsantosti/projeto-html5-s',
+            techs: [
+                'HTML5', 'CSS3', 'JQUERY'
+            ]
+        });
+        const repository = response.data;
+        setRepositories([...repositories, repository]);
+    }
+
+    async function handleRemoveRepository(id) {
+        const items = [...repositories];
+        const index = items.findIndex(item => item.id === id);
+
+        if(index !== -1){
+            items.splice(index, 1);
+            const response = await api.delete(`repositories/${id}`);
+            if(response.status === 204){
+                setRepositories(items);
+            }
+        }
+    }
+
+    return (
+        <div>
+            <ul data-testid="repository-list" className="repository-list">
+                {repositories.map(repository => (
+                    <li key={repository.id} className="repository-list-item">
+                        <span className="repository-list-item-title">{repository.title}</span>
+                        <button onClick={() => handleRemoveRepository(repository.id)}>Remover</button>
+                    </li>
+                ))}
+            </ul>
+            <button onClick={handleAddRepository}>Adicionar</button>
+        </div>
+    );
 }
 
 export default App;
